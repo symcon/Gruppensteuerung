d3b3377bd2d5650773a16dab2d67a207125fdd99
Till Brede <tb@symcon.de>
2020-04-08 15:37:39 +0200

    added function to chck the status

--- Gruppensteuerung/module.php
+++ Gruppensteuerung/module.php
@@ -27,14 +27,6 @@ declare(strict_types=1);
 
             $variables = json_decode($this->ReadPropertyString('Variables'), true);
 
-            if (!$this->ReadPropertyBoolean('Active')) {
-                $this->SetStatus(104);
-                return;
-            }
-
-            if (count($variables) <= 0) {
-                $this->SetStatus(204);
-            }
             //Register references
             foreach ($this->GetReferenceList() as $referenceID) {
                 $this->UnregisterReference($referenceID);
@@ -44,40 +36,11 @@ declare(strict_types=1);
                 $this->RegisterReference($variable['VariableID']);
             }
 
-            //Check if all variables are the same type
-            $referenceVariable = IPS_GetVariable($variables[0]['VariableID']);
-            $referenceType = $referenceVariable['VariableType'];
-            if (count($variables) > 1) {
-                foreach ($variables as $variable) {
-                    if (($referenceType != IPS_GetVariable($variable['VariableID'])['VariableType'])
-                    ) {
-                        $this->SetStatus(200);
-                        return;
-                    }
-                }
-            }
-
-            //Check if all variables have the same profile
-            foreach ($variables as $variable) {
-                if (!$this->sameProfile($variable['VariableID'])) {
-                    $this->SetStatus(201);
-                }
-            }
-
-            //Check if all variables have an action
-            foreach ($variables as $variable) {
-                if (!HasAction($variable['VariableID'])) {
-                    $this->SetStatus(202);
-                    return;
-                }
-            }
-
-            //Check if all variables exist
-            foreach ($variables as $variable) {
-                if (!IPS_VariableExists($variable['VariableID'])) {
-                    $this->SetStatus(203);
-                    return;
-                }
+            $computedStatus = $this->computeStatus();
+            $this->SetStatus($computedStatus);
+            //Return if instance has issues
+            if ($computedStatus != 102) {
+                return;
             }
 
             //Register messages
@@ -88,8 +51,6 @@ declare(strict_types=1);
             foreach ($variables as $variable) {
                 $this->RegisterMessage($variable['VariableID'], VM_UPDATE);
             }
-
-            $this->SetStatus(102);
 
             //Register variable of needed type with correct profile
             $variableProfile = $referenceVariable['VariableCustomProfile'];
@@ -148,21 +109,66 @@ declare(strict_types=1);
                 return false;
             }
             return true;
+        private function computeStatus()
+        {
+            //Active
+            if (!$this->ReadPropertyBoolean('Active')) {
+                return 104;
+            }
+
+            $variables = json_decode($this->ReadPropertyString('Variables'), true);
+            $referenceVariableID = $variables[0]['VariableID'];
+
+            //List empty
+            if (count($variables) <= 0) {
+                return 204;
+            }
+
+            //Exist
+            foreach ($variables as $variable) {
+                if (!IPS_VariableExists($variable['VariableID'])) {
+                    return 203;
+                }
+            }
+
+            //Same type
+            if (count($variables) > 1) {
+                foreach ($variables as $variable) {
+                    if (($this->getType($referenceVariableID) != $this->getType($variable['VariableID']))) {
+                        return 200;
+                    }
+                }
+            }
+
+            //Same profile
+            foreach ($variables as $variable) {
+                if ($this->getProfile($variable['VariableID']) != $this->getProfile($referenceVariableID)) {
+                    return 201;
+                }
+            }
+
+            //Have action
+            foreach ($variables as $variable) {
+                if (!HasAction($variable['VariableID'])) {
+                    return 202;
+                }
+            }
+
+            //Everything ok
+            return 102;
         }
 
         private function SwitchGroup($value)
         {
-            if ($this->GetStatus() == 102) {
-                if ($value != $this->GetValue('Status')) {
-                    $this->SetValue('Status', $value);
-                    $variables = json_decode($this->ReadPropertyString('Variables'), true);
+            if (($this->computeStatus() == 102) && ($value != $this->GetValue('Status'))) {
+                $this->SetValue('Status', $value);
+                $variables = json_decode($this->ReadPropertyString('Variables'), true);
 
-                    foreach ($variables as $variable) {
-                        if (GetValue($variable['VariableID']) != $value && HasAction($variable['VariableID'])) {
-                            RequestAction($variable['VariableID'], $value);
-                        } elseif (!HasAction($variable['VariableID'])) {
-                            throw new Exception('One variable has no action.');
-                        }
+                foreach ($variables as $variable) {
+                    if (GetValue($variable['VariableID']) != $value && HasAction($variable['VariableID'])) {
+                        RequestAction($variable['VariableID'], $value);
+                    } elseif (!HasAction($variable['VariableID'])) {
+                        throw new Exception('One variable has no action.');
                     }
                 }
             }
